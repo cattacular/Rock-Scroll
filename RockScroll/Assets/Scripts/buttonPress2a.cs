a393e18efac16e43864bec518da20f8f1536b7e3
Simone-lee <148592608+Simone-lee@users.noreply.github.com>
2023-11-14 21:37:38 -0600

    Main Menu Fixed up

---
+++ RockScroll/Assets/Scripts/buttonPress2a.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonPress2a: MonoBehaviour
+{
+    public GameObject ButtonPressed;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SceneManager.LoadScene("Level1");
+    }
+
+    private void CancelInvoke()
+    {
+        SceneManager.LoadScene("Esteem Rank - Main Menu");
+    }
+}
--- RockScroll/Assets/Scripts/buttonPressb.cs
+++ RockScroll/Assets/Scripts/buttonPressb.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class buttonPress2 : MonoBehaviour
+public class buttonPressb : MonoBehaviour
 {
     public GameObject ButtonPressed;
 
